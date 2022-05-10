6d6d82639c1adb7e5cbb4f36f1fd270184798b44
matyaspr <matyaspr@gmail.com>
2022-05-10 17:16:46 -0300

    ADD frontend

---
+++ public/precache-manifest.587bf44b252d28098ce595d23a5acd0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3616229b72f42ae17e76744327fc6c82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c5febbb58edf745d93",
+    "url": "/static/css/2.a0bde1ba.chunk.css"
+  },
+  {
+    "revision": "a446fca1c322b697966d",
+    "url": "/static/css/main.703906b6.chunk.css"
+  },
+  {
+    "revision": "a6c5febbb58edf745d93",
+    "url": "/static/js/2.293cf3c5.chunk.js"
+  },
+  {
+    "revision": "afb90854ca170d941f072bc07eb8c0b6",
+    "url": "/static/js/2.293cf3c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a446fca1c322b697966d",
+    "url": "/static/js/main.2f3f8944.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
